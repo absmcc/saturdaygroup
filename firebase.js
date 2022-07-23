d5c4d608c815cb136dc1e8ccc897cc0202bbcc60
Maria Redman <mariaredman@Marias-MacBook-Air-2.local>
2022-07-23 15:05:46 -0400

    Firebase Master

--- firebase.js
+++ firebase.js
@@ -1,1 +1,26 @@
+import{initializeApp, cert} from "firebase-admin/app"
+import {getFirestore} from "firebase-admin/firestore"
+import { credentials } from "./credentials.js"
 
+initializeApp({
+credential: cert(credentials)
+
+})
+
+const db = getFirestore()
+
+// //ready to work in the db
+
+// console.log("database is ready")
+
+db.collection("cars").get()
+.then(moviesCollection=>{
+
+moviesCollection.docs.forEach(movie => {
+   
+    console.log(movie.data())
+   });
+})
+.catch(err=>{
+    console.error(err)
+})
